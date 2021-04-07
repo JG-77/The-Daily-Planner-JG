87357462f1b0aeca573fbf45ae3d53168ccc5fde
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-04-06 21:36:37 -0700

    Edited html by removing most time blocks. Attempting to recreate them through jquery in Javascript using bootstrap.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,36 @@
 var currentDayEl = $('#currentDay');
+var timeContainer = $('#container');
 
+//creating timeblock elelemtns 
+var timeDivs = $('<div>');
+timeDivs.addClass('input-group');
+timeContainer.append(timeDivs);
+
+var timeHours = [
+    '9 A.M.',
+    '10 A.M.',
+    '11 A.M.',
+    '12 P.M.',
+    '1 P.M.',
+    '2 P.M.',
+    '3 P.M.',
+    '4 P.M.',
+    '5 P.M.',
+];
+
+for (var i = 0; i < timeHours.length; i++) {
+    var timeBlocksEl = $('<span>');
+    timeBlocksEl.text(timeHours[i]);
+    timeBlocksEl.addClass('input-group-text');
+    timeDivs.append(timeBlocksEl);
+}
+
+
+var currentHour = parseInt(moment().format("H"));
+console.log(currentHour);
+
+
+//Displays current date and time on jumbotron
 function currentDayAndTime() {
     var currently = moment().format('[Today is] MMM DD, YYYY [at] hh:mm:ss a');
     currentDayEl.text(currently);
@@ -7,3 +38,4 @@ function currentDayAndTime() {
 }
 
 setInterval(currentDayAndTime, 1000);
+
